c5eaaa408f6037dc386d739f1bad1bb202a63413
Silvio Coelho <conejofoz@gmail.com>
2024-09-06 14:58:33 -0300

    usando a função get_list_or_404 para listar as categorias e retornar um 404 caso não encontre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from django.http import Http404, HttpResponse
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
@@ -13,11 +13,15 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True)
+    """ recipes = Recipe.objects.filter(category__id=category_id, is_published=True)
     title = recipes.first().category.name
 
     if not recipes:
-        raise Http404('No recipes found')
+        raise Http404('No recipes found') """
+    
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True))
+
+    title = recipes[0].category.name # como o nome diz, retorna uma lista
     
     return render(request, 'recipes/pages/category.html', 
                     context={
