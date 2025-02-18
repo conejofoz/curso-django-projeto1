cf058a3447a5c681bbba48eddd0125b7141a99dd
Silvio Coelho <conejofoz@gmail.com>
2025-02-18 15:55:33 -0300

    corrigindo a senha que não estava sendo salva com criptografia

--- authors/views.py
+++ authors/views.py
@@ -24,7 +24,9 @@ def register_create(request):
     form = RegisterForm(POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Seu usuário foi criado, por favor faça login.') # falta implementar no html
         del(request.session['register_form_data'])
     return redirect('authors:register')
