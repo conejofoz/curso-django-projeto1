b3b9341f2d0444f8ffd82471250589715db52c4f
Silvio Coelho <conejofoz@gmail.com>
2025-02-25 11:02:35 -0300

    refatorando a paginação para poder ser usada or outras views

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,8 @@ from django.http import Http404, HttpResponse
 from django.contrib import messages
 from django.core.paginator import Paginator
 from utils.recipes.factory import make_recipe
-from utils.pagination import make_pagination_range
+# from utils.pagination import make_pagination_range
+from utils.pagination import make_pagination
 from recipes.models import Recipe
 
 
@@ -18,18 +19,7 @@ def home(request):
     messages.error(request, 'Mensagem de erro!')
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    try:
-        current_page = int(request.GET.get('page', 1))
-    except ValueError:
-        current_page = 1
-
-    paginator = Paginator(recipes, 6)
-    page_obj = paginator.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        4,
-        current_page)
+    page_obj, pagination_range = make_pagination(request, recipes, 6, 4)
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
